2c08b2d688cda0715d60ddd8bde1c652f7de0069
julian0becker <julianentwickelt@gmail.com>
2019-06-16 10:11:25 +0200

    frontend apollo set up and routes setup

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,22 +1,18 @@
-const { AuthenticationError, UserInputError } = require('apollo-server');
-
-const checkAuth = require('../../util/check-auth');
-const Post = require('../../models/Post');
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
+const { UserInputError } = require("apollo-server");
 
 module.exports = {
   Mutation: {
     createComment: async (_, { postId, body }, context) => {
       const { username } = checkAuth(context);
-      if (body.trim() === '') {
-        throw new UserInputError('Empty comment', {
-          errors: {
-            body: 'Comment body must not empty'
-          }
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: { body: "comment must not be empty" }
         });
       }
 
       const post = await Post.findById(postId);
-
       if (post) {
         post.comments.unshift({
           body,
@@ -25,26 +21,7 @@ module.exports = {
         });
         await post.save();
         return post;
-      } else throw new UserInputError('Post not found');
-    },
-    async deleteComment(_, { postId, commentId }, context) {
-      const { username } = checkAuth(context);
-
-      const post = await Post.findById(postId);
-
-      if (post) {
-        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
-
-        if (post.comments[commentIndex].username === username) {
-          post.comments.splice(commentIndex, 1);
-          await post.save();
-          return post;
-        } else {
-          throw new AuthenticationError('Action not allowed');
-        }
-      } else {
-        throw new UserInputError('Post not found');
-      }
+      } else throw new UserInputError("Post not found");
     }
   }
 };
